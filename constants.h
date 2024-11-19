9063e1f1c28b182b0b9961850a29cb59f7b264d1
AndyTZX <andytan786@hotmail.com>
2024-11-19 16:00:41 +0800

    updated check_winner and button_clicked

--- constants.h
+++ constants.h
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h> 
+#include <stdbool.h>
 
 typedef struct { // Define a struct called GameData
     GtkWidget *score_x_label; // Declare a GtkWidget pointer called score_x_label
@@ -12,6 +13,7 @@ char player = 'X';
 char board[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 GtkWidget *buttons[9]; // Array to store the button widgets
 GtkWidget *back_button; // Declare a GtkWidget pointer called back_button
+GtkWidget *end_button; // Declare a GtkWidget pointer called end_button
 GtkWidget *status_label; // Declare a GtkWidget pointer called status_label
 
 char player_x_label[10]; // Declare a string called player_x_label and set it to "PLAYER X"
@@ -25,6 +27,7 @@ static guint blink_timeout_id = 0;
 GameData *game_data; // Declare a pointer to a GameData struct called game_data
 gboolean auto_reset_pending = FALSE;
 gboolean blink_winner(gpointer data);
+gboolean delayed_reset(gpointer data);
 
 extern GtkWidget *main_menu_window; // Declare a GtkWidget pointer called main_menu_window
 
@@ -63,45 +66,34 @@ void reset_board() {
 }
 
 
-char check_winner() {
-    // Check rows
-    for (int i = 0; i < 3; i++) {
-        if (board[i * 3] == board[i * 3 + 1] && board[i * 3 + 1] == board[i * 3 + 2] && board[i * 3] != ' ') {
-            winning_indices[0] = i * 3;
-            winning_indices[1] = i * 3 + 1;
-            winning_indices[2] = i * 3 + 2;
-            return board[i * 3];
+bool checkWin(char player) {
+    // Winning combinations (rows, columns, diagonals)
+    int winCombos[8][3] = {
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8},  // Rows
+        {0, 3, 6}, {1, 4, 7}, {2, 5, 8},  // Columns
+        {0, 4, 8}, {2, 4, 6}              // Diagonals
+    };
+
+    // Check if any winning combination is met
+    for (int i = 0; i < 8; i++) { //each winning combination in winCombos array
+        if (board[winCombos[i][0]] == player && // check if all 3 cells in the contained in the combination contain the the specified player's symbol(X or O)
+            board[winCombos[i][1]] == player &&
+            board[winCombos[i][2]] == player) {
+            winning_indices[0] = winCombos[i][0]; // 1st cell of the i-the winning combination in the array, the value is assigned to winning_indices[0] 
+            winning_indices[1] = winCombos[i][1]; // 2nd cell 
+            winning_indices[2] = winCombos[i][2]; // 3rd cell
+            return true;
         }
     }
-    // Check columns
-    for (int i = 0; i < 3; i++) { // Loop through the columns
-        if (board[i] == board[i + 3] && board[i + 3] == board[i + 6] && board[i] != ' ') { // Check if the columns are equal
-            winning_indices[0] = i; // top cell of the column
-            winning_indices[1] = i + 3; // middle cell of the column 
-            winning_indices[2] = i + 6; // bottom cell of the column
-            return board[i]; // Return the player
+    return false;
+}
+bool checkTie() {
+    for (int i = 0; i < 9; i++) {
+        if (board[i] == ' ') {
+            return false; // There is at least one empty cell, so it's not a tie
         }
     }
-    // Check diagonals
-    if (board[0] == board[4] && board[4] == board[8] && board[0] != ' ') { // Check the diagonal from top-left to bottom-right
-        winning_indices[0] = 0; // top-left cell
-        winning_indices[1] = 4; // middle cell
-        winning_indices[2] = 8; // bottom-right cell
-        return board[0]; // Return the player
-    }
-    if (board[2] == board[4] && board[4] == board[6] && board[2] != ' ') {
-        winning_indices[0] = 2; // top-right cell
-        winning_indices[1] = 4; // middle cell
-        winning_indices[2] = 6; // bottom-left cell
-        return board[2]; // Return the player
-    }
-    // Check for draw
-    for (int i = 0; i < 9; i++) { 
-        if (board[i] == ' ') { // Check if there are any empty cells
-            return ' '; // Game is still ongoing
-        }
-    }
-    return 'D'; // Draw
+    return true; // All cells are filled, so it's a tie
 }
 
 gboolean auto_reset_board(gpointer data) {
@@ -116,6 +108,20 @@ gboolean auto_reset_board(gpointer data) {
     }
     return FALSE; // Don't repeat the timer
 }
+
+// Define the delayed_reset function
+gboolean delayed_reset(gpointer data) {
+    // Your reset logic here
+    reset_board();
+    // Re-enable all buttons
+    for (int i = 0; i < 9; i++) {
+        gtk_widget_set_sensitive(buttons[i], TRUE);
+        gtk_widget_set_name(buttons[i], "grid-button");
+    }
+    auto_reset_pending = FALSE;
+    return FALSE; // Don't repeat the timer
+}
+
 
 // Function to blink the winning indices
 gboolean blink_winner(gpointer data) {
@@ -133,53 +139,50 @@ gboolean blink_winner(gpointer data) {
     return TRUE; // blink_winner will called again after a delay to create a blinking effect
 }
 
-void button_clicked(GtkWidget *widget, gpointer data) {
-    int index = GPOINTER_TO_INT(data); // Convert the data pointer to an integer
-    if (board[index] == ' ') { // Check if the board at the index is empty
+void button_clicked(GtkWidget *button, gpointer data) {
+    int index = GPOINTER_TO_INT(data);
+    if (board[index] == ' ') {
         board[index] = player;
-        
-        char player_str[2] = {player, '\0'}; // Create a string with the player character
-        gtk_button_set_label(GTK_BUTTON(widget), player_str); // Set the label of the button to the player
-
-        // Set the name of the widget
+        char label[2] = {player, '\0'};
+        gtk_button_set_label(GTK_BUTTON(button), label);
         if (player == 'X') {
-            gtk_widget_set_name(widget, "button-x"); //extract button-x style from css
+            gtk_widget_set_name(button, "button-x");
         } else {
-            gtk_widget_set_name(widget, "button-o"); //extract button-o style from css
+            gtk_widget_set_name(button, "button-o");
         }
 
-        char winner = check_winner();
-        if (winner != ' ') { // Check if there is a winner
+        if (checkWin(player)) {
             char message[50];
-            if (winner == 'D') { // Check if the game is a draw
-                snprintf(message, sizeof(message), "It's a draw!"); 
-                score_ties++;
-            } else { // If there is a winner
-                snprintf(message, sizeof(message), "Player %c wins!", winner);
-                if (winner == 'X') { // Check if the winner is X
-                    score_x++;
-                } else { // If the winner is O
-                    score_o++;
-                }
-                blink_timeout_id = g_timeout_add(500, blink_winner, NULL); // Add a timeout to blink the winning indices
+            snprintf(message, sizeof(message), "Player %c wins!", player);
+            if (player == 'X') {
+                score_x++;
+            } else {
+                score_o++;
             }
-            gtk_label_set_text(GTK_LABEL(status_label), message); // Set the text of the status_label widget
-            update_scoreboard(); // Update the scoreboard
-            
-            // Disable all buttons
+            blink_timeout_id = g_timeout_add(500, blink_winner, NULL); // Add a timeout to blink the winning indices
+            g_timeout_add_seconds(2, delayed_reset, NULL); // Schedule a delayed reset after 2 seconds for a win
+            gtk_label_set_text(GTK_LABEL(status_label), message);
+            update_scoreboard();
+            gtk_widget_set_sensitive(end_button, FALSE);
             for (int i = 0; i < 9; i++) {
-                gtk_widget_set_sensitive(buttons[i], FALSE); // Disable the button
+                gtk_widget_set_sensitive(buttons[i], FALSE);
             }
-
-        // Schedule the board reset after 3 seconds
-            auto_reset_pending = TRUE;
-            g_timeout_add_seconds(2, auto_reset_board, NULL);
-            
+        } else if (checkTie()) {
+            char message[50];
+            snprintf(message, sizeof(message), "It's a draw!");
+            score_ties++;
+            g_timeout_add_seconds(2, delayed_reset, NULL); // Schedule a delayed reset after 2 seconds for a draw
+            gtk_label_set_text(GTK_LABEL(status_label), message);
+            update_scoreboard();
+            gtk_widget_set_sensitive(end_button, FALSE);
+            for (int i = 0; i < 9; i++) {
+                gtk_widget_set_sensitive(buttons[i], FALSE);
+            }
         } else {
-            player = (player == 'X') ? 'O' : 'X'; // Switch players
-            char message[50]; 
-            snprintf(message, sizeof(message), "Player %c's turn", player); // Set the message
-            gtk_label_set_text(GTK_LABEL(status_label), message); // Set the text of the status_label widget
+            player = (player == 'X') ? 'O' : 'X';
+            char message[50];
+            snprintf(message, sizeof(message), "Player %c's turn", player);
+            gtk_label_set_text(GTK_LABEL(status_label), message);
         }
     }
 }
